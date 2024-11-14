1c12ce408ff8f4593a356818f6d97305b5692cf7
Azim19062006 <mamatbaevazim06@gmail.com>
2024-11-14 12:56:05 +0600

    created Product Card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import {ProductCard} from "./components/ProductCard";
+
+const products = [
+  {
+    id: 0,
+    name: "Product 1",
+    category: "Category 1",
+    price: 200,
+    img: "https://m.media-amazon.com/images/I/619f09kK7tL._AC_UF894,1000_QL80_.jpg"
+  },
+  {
+    id: 1,
+    name: "Product 2",
+    category: "Category 1",
+    price: 500,
+    img: "https://m.media-amazon.com/images/I/619f09kK7tL._AC_UF894,1000_QL80_.jpg"
+  }
+]
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      {
+        products.map((product) => (
+          <ProductCard data = {product}/>
+        ))}
+    </div>
+  );
 }
 
 export default App;
